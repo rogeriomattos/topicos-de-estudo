97d209cbc3d33a5df548909d385c9181575d5cc4
Rogerio Almeida <rogeriomattos38@outlook.com>
2022-02-18 17:27:15 -0300

    Algoritmo para converter decimal to binario usando pilha

---
+++ estruturaDeDados/01_pilha/exercicios/convertIntToBinary.c
@@ -0,0 +1,73 @@
+/*
+  Crie um programa que lê um número inteiro e converta ele para binário, usando pilha.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+
+typedef struct
+{
+  int topo;
+  int max;
+  Item *item;
+} * Pilha;
+
+Pilha createPilha(int maxLength)
+{
+  Pilha p = malloc(sizeof(Pilha));
+  p->max = maxLength;
+  p->topo = -1;
+  p->item = malloc(sizeof(Item));
+}
+
+void insertPilha(Item item, Pilha p)
+{
+  p->topo++;
+  p->item[p->topo] = item;
+}
+
+Item removePilha(Pilha p)
+{
+  Item x = getTopo(p);
+  p->topo--;
+
+  return x;
+}
+
+Item getTopo(Pilha p)
+{
+  return p->item[p->topo];
+}
+
+int pilhaVazia(Pilha p)
+{
+  if (p->topo == -1)
+    return 1;
+
+  return 0;
+}
+
+void main()
+{
+  Pilha p = createPilha(32);
+  int number;
+  printf("Digite um numero: ");
+  scanf("%d", &number);
+  int aux = number;
+
+  while (aux > 0)
+  {
+    insertPilha(aux % 2, p);
+    aux = aux / 2;
+  }
+
+  printf("%d = ", number);
+
+  do
+  {
+
+    int f = removePilha(p);
+    printf("%d", f);
+  } while (!pilhaVazia(p));
+}
